a2af50f015ab6e21cc8732026343d07b8ba2cff1
AraHaan <seandhunt_7@yahoo.com>
2015-10-21 07:03:09 -0500

    Added Support for C++ 2005 Express (8)

---
+++ 2005/include/zlibEx/zlibEx.h
@@ -0,0 +1,39 @@
+/* zlibEx.h -- interface of the zlib Extended multi purpose compression & file  I/O library
+  version 1.2.9, September 19th, 2015
+
+  Copyright (C) 2015 Sean Hunt
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+  
+*/
+#ifndef ZLIBEX_H
+#define ZLIBEX_H
+#include "..\zlib\zlib.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+#define ZLIBEX_VERSION "1.2.9"
+#define ZLIBEX_VERNUM 0x1290
+#define ZLIBEX_VER_MAJOR 1
+#define ZLIBEX_VER_MINOR 2
+#define ZLIBEX_VER_REVISION 9
+#define ZLIBEX_VER_SUBREVISION 0
+#endif
+
+#ifdef __cplusplus
+}
+#endif /* ZLIBEX_H */
---
+++ README.md
@@ -0,0 +1,16 @@
+# zlibEx
+
+![zlib Extended](https://i.gyazo.com/87a5b318e3ddc447d8008292ac44f835.png)
+
+A DLL made in C++ 6 and now also for C++ 2005 Express as well (2005 uses up to date libxml though as I could not do so in C++ 6).
+
+
+
+# Requirements
+*	C++ 6 (for building for Visual Studio 6).
+*	C++ 2005 Express (For not building for Visual Studio 6).
+*	MFC installed on both. (optional)
+
+# Other
+to build libogg I had to copy some of the MFC Header fiels from C++ 6 to the project folder for 8 on it.
+So use that if you dont have MFC for it. if you do then you can remove those files.
